f74bb8d5530c01f4c532ebb8c305ea7aeee7cc4a
igaurav07 <gaurav2021muz@gmail.com>
2026-07-08 12:04:56 +0530

    Time: 74 ms (31.09%), Space: 156.7 MB (48.71%) - LeetHub

--- 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
+++ 3756-concatenate-non-zero-digits-and-multiply-by-sum-ii/3756-concatenate-non-zero-digits-and-multiply-by-sum-ii.cpp
@@ -2,54 +2,56 @@ class Solution {
 public:
     vector<int> sumAndMultiply(string s, vector<vector<int>>& queries) {
         int m = s.size();
-        vector<int> positions;
-        vector<int> digits;
+        vector<int> pos, digs;
+        pos.reserve(m);
+        digs.reserve(m);
+        
         for (int i = 0; i < m; i++) {
             if (s[i] != '0') {
-                positions.push_back(i);
-                digits.push_back(s[i] - '0');
+                pos.push_back(i);
+                digs.push_back(s[i] - '0');
             }
         }
-        int t = digits.size();
+        
+        int t = digs.size();
         const long long MOD = 1000000007LL;
         
-        vector<long long> pow10(t + 1, 1LL);
+        vector<long long> pow10(t + 1, 1);
         for (int i = 1; i <= t; i++) {
-            pow10[i] = pow10[i - 1] * 10LL % MOD;
+            pow10[i] = pow10[i - 1] * 10 % MOD;
         }
         
-        vector<long long> prefix_num(t + 1, 0LL);
+        vector<long long> pre(t + 1, 0);
         for (int i = 0; i < t; i++) {
-            prefix_num[i + 1] = (prefix_num[i] * 10LL + digits[i]) % MOD;
+            pre[i + 1] = (pre[i] * 10 + digs[i]) % MOD;
         }
         
-        vector<long long> prefix_sum(t + 1, 0LL);
+        vector<long long> presum(t + 1, 0);
         for (int i = 0; i < t; i++) {
-            prefix_sum[i + 1] = prefix_sum[i] + digits[i];
+            presum[i + 1] = presum[i] + digs[i];
         }
         
         vector<int> ans;
+        ans.reserve(queries.size());
+        
         for (auto& q : queries) {
             int l = q[0], r = q[1];
             
-            // Find first non-zero position >= l
-            auto it1 = lower_bound(positions.begin(), positions.end(), l);
-            int L = it1 - positions.begin();
+            auto it1 = lower_bound(pos.begin(), pos.end(), l);
+            int L = it1 - pos.begin();
             
-            // Find last non-zero position <= r
-            auto it2 = upper_bound(it1, positions.end(), r);
-            int R = (it2 - positions.begin()) - 1;
+            auto it2 = upper_bound(it1, pos.end(), r);
+            int R = (it2 - pos.begin()) - 1;
             
             if (L > R) {
                 ans.push_back(0);
                 continue;
             }
             
-            long long len = R - L + 1LL;
-            long long xmod = (prefix_num[R + 1] - prefix_num[L] * pow10[len] % MOD + MOD) % MOD;
-            long long sm = prefix_sum[R + 1] - prefix_sum[L];
-            long long res = xmod * (sm % MOD) % MOD;
-            ans.push_back((int)res);
+            long long len = R - L + 1;
+            long long x = (pre[R + 1] - pre[L] * pow10[len] % MOD + MOD) % MOD;
+            long long sm = presum[R + 1] - presum[L];
+            ans.push_back(x * (sm % MOD) % MOD);
         }
         return ans;
     }
